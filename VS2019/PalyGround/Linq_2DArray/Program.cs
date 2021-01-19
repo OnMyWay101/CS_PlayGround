65c12a425effe1378e40e047565a10afbae4bc43
unknown <wangcj@sinodsp.com>
2021-01-19 17:37:06 +0800

    add vs2019 solution

---
+++ VS2019/PalyGround/Linq_2DArray/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace Linq_2DArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[4, 4];
+            for (int i = 0; i < 4; i++)
+                for (int j = 0; j < 4; j++)
+                    array[i, j] = (i + 1) * (j + 1);
+
+            //var intQuery = from i in array.Cast<int>()
+            //                where i > 5
+            //                select i;
+
+            //foreach (int i in intQuery)
+            //    Console.WriteLine(i);
+            var intQuery = array.Cast<int>().Max();
+      
+            Console.WriteLine(intQuery);
+        }
+    }
+}
